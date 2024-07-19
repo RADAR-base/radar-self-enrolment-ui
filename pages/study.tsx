bedd22d280a0d5ad2fa2bcc10e66ba7773b95a62
Pauline <mpgxvii@gmail.com>
2024-07-19 19:46:25 +0100

    Get study info externally

--- pages/study.tsx
+++ pages/study.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+import { studyInfo } from "../data/study-questionnaire"
 
 // Import render helpers
 import { MarginCard, CardTitle, TextCenterButton, InnerCard } from "../pkg"
@@ -9,6 +10,7 @@ import { MarginCard, CardTitle, TextCenterButton, InnerCard } from "../pkg"
 // Renders the eligibility page
 const Study: NextPage = () => {
   const router = useRouter()
+  const projectId = "EDIFY"
 
   useEffect(() => {
     // If the router is not ready yet, or we already have a flow, do nothing.
@@ -21,19 +23,11 @@ const Study: NextPage = () => {
     <>
       <Head>
         <title>Welcome</title>
-        <meta name="description" content="NextJS + React + Vercel + Ory" />
       </Head>
       <MarginCard>
-        <CardTitle>EDIFY Research Study</CardTitle>
+        <CardTitle>{projectId} Research Study</CardTitle>
         <img src="image.png" />
-        <div className="center">
-          <label className="inputLabel">Study Overview</label>
-          <InnerCard>
-            This study aims to understand the effects of eating disorders.
-            Participants will be askeed to fill out questionnaires and undergo
-            non-invaise tests over the course of one year.
-          </InnerCard>
-        </div>
+        <StudyInfo questions={studyInfo} />
         <TextCenterButton
           className=""
           disabled=""
@@ -45,6 +39,27 @@ const Study: NextPage = () => {
         {/* <Flow onSubmit={onSubmit} flow={flow} /> */}
       </MarginCard>
     </>
+  )
+}
+
+const StudyInfo: React.FC<any> = ({ questions }) => {
+  return (
+    <div className="center">
+      {questions.map((question, index) => {
+        if (question.field_type === "info") {
+          return (
+            question.select_choices_or_calculations instanceof Array &&
+            question.select_choices_or_calculations.map((info, idx) => (
+              <div key={`${index}-${idx}`}>
+                <label className="inputLabel">{info.code}</label>
+                <InnerCard>{info.label}</InnerCard>
+              </div>
+            ))
+          )
+        }
+        return null
+      })}
+    </div>
   )
 }
 
