bea37b8c13a66de144f22fa734630622f1479623
this-Aditya <adityamishra2217@gmail.com>
2024-08-23 20:53:51 +0530

    Formatted code and removed log

--- config/github-config.ts
+++ config/github-config.ts
@@ -1,29 +1,37 @@
 export const GITHUB_CONFIG = {
-    ORGANIZATION_NAME: 'RADAR-base',
-    REPOSITORY_NAME: process.env.GITHUB_REPO_NAME || 'radar-self-enrolment-definitions',
-    DEFINITIONS_BRANCH: process.env.GITHUB_REPO_BRANCH_NAME || 'test',
-    MAX_CONTENT_LENGTH: parseInt(process.env.GITHUB_RESPONSE_CONTENT_LENGTH || '1000000', 10),
-    CACHE_DURATION: parseInt(process.env.GITHUB_RESPONSE_CACHE_DURATION || '180000', 10),
-    CACHE_SIZE: parseInt(process.env.GITHUB_RESPONSE_CACHE_SIZE || '50', 10),
-    API_URL: 'https://api.github.com',
-    ACCEPT_HEADER: 'application/vnd.github.v3+json',
+  ORGANIZATION_NAME: "RADAR-base",
+  REPOSITORY_NAME:
+    process.env.GITHUB_REPO_NAME || "radar-self-enrolment-definitions",
+  DEFINITIONS_BRANCH: process.env.GITHUB_REPO_BRANCH_NAME || "test",
+  MAX_CONTENT_LENGTH: parseInt(
+    process.env.GITHUB_RESPONSE_CONTENT_LENGTH || "1000000",
+    10,
+  ),
+  CACHE_DURATION: parseInt(
+    process.env.GITHUB_RESPONSE_CACHE_DURATION || "180000",
+    10,
+  ),
+  CACHE_SIZE: parseInt(process.env.GITHUB_RESPONSE_CACHE_SIZE || "50", 10),
+  API_URL: "https://api.github.com",
+  ACCEPT_HEADER: "application/vnd.github.v3+json",
 }
 
 export const GITHUB_AUTH_CONFIG = {
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '',
+  GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "",
 }
 
 export const REMOTE_DEFINITIONS_CONFIG = {
-    CONSENT_VERSION: 'v1',
-    ELIGIBILITY_VERSION: 'v1',
-    STUDY_INFO_VERSION: 'v1',
+  CONSENT_VERSION: "v1",
+  ELIGIBILITY_VERSION: "v1",
+  STUDY_INFO_VERSION: "v1",
 
-    CONSENT_DEFINITION_FILE_NAME_CONTENT: 'consent',
-    ELIGIBILITY_DEFINITION_FILE_NAME_CONTENT: 'eligibility',
-    STUDY_INFO_DEFINITION_FILE_NAME_CONTENT: 'study_info'
+  CONSENT_DEFINITION_FILE_NAME_CONTENT: "consent",
+  ELIGIBILITY_DEFINITION_FILE_NAME_CONTENT: "eligibility",
+  STUDY_INFO_DEFINITION_FILE_NAME_CONTENT: "study_info",
 }
 
 export const MP_CONFIG = {
-    BASE_URL: process.env.MP_CONFIG_BASE_URL || 'http://127.0.1.1:8080/managementportal',
-    PROJECTS_ENDPOINT: process.env.MP_PROJECTS_ENDPOINT || 'api/public/projects',
+  BASE_URL:
+    process.env.MP_CONFIG_BASE_URL || "http://127.0.1.1:8080/managementportal",
+  PROJECTS_ENDPOINT: process.env.MP_PROJECTS_ENDPOINT || "api/public/projects",
 }
--- utils/structures.ts
+++ utils/structures.ts
@@ -1,38 +1,38 @@
 export type SelectChoice = {
-    code: string;
-    label: string;
-};
+  code: string
+  label: string
+}
 
 export type Definition = {
-    field_name: string;
-    form_name: string;
-    section_header: string;
-    field_type: string;
-    field_label: string;
-    select_choices_or_calculations: SelectChoice[];
-    field_note: string;
-    text_validation_type_or_show_slider_number: string;
-    text_validation_min: string;
-    text_validation_max: string;
-    identifier: string;
-    branching_logic: string;
-    required_field: string;
-    custom_alignment: string;
-    question_number: string;
-    matrix_group_name: string;
-    matrix_ranking: string;
-    field_annotation: string;
-};
+  field_name: string
+  form_name: string
+  section_header: string
+  field_type: string
+  field_label: string
+  select_choices_or_calculations: SelectChoice[]
+  field_note: string
+  text_validation_type_or_show_slider_number: string
+  text_validation_min: string
+  text_validation_max: string
+  identifier: string
+  branching_logic: string
+  required_field: string
+  custom_alignment: string
+  question_number: string
+  matrix_group_name: string
+  matrix_ranking: string
+  field_annotation: string
+}
 
 export type Project = {
-    projectName: string;
-    description?: string;
-    sourceTypes: SourceType[];
+  projectName: string
+  description?: string
+  sourceTypes: SourceType[]
 }
 
 export type SourceType = {
-    id: number;
-    model?: string;
-    producer?: string;
-    catalogVersion?: string;
+  id: number
+  model?: string
+  producer?: string
+  catalogVersion?: string
 }
