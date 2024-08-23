aaf01bee08bdd02b097d29b4c1fc58ba225ad071
this-Aditya <adityamishra2217@gmail.com>
2024-08-23 20:53:51 +0530

    Formatted code and removed log

--- pages/study.tsx
+++ pages/study.tsx
@@ -1,28 +1,33 @@
-import type {GetServerSideProps, NextPage} from "next"
+import type { GetServerSideProps, NextPage } from "next"
 import Head from "next/head"
 import { useRouter } from "next/router"
-import {MutableRefObject, useEffect, useRef, useState} from "react"
+import { MutableRefObject, useEffect, useRef, useState } from "react"
 
+import { REMOTE_DEFINITIONS_CONFIG } from "../config/github-config"
 // Import render helpers
 import { MarginCard, CardTitle, TextCenterButton, InnerCard } from "../pkg"
-import githubService from "../services/github-service";
-import {REMOTE_DEFINITIONS_CONFIG} from "../config/github-config";
-import {Definition, Project} from "../utils/structures";
-import fetchProjectsFromMp from "../services/mp-projects-fetcher";
-import {MPFetchError} from "../utils/errors/MPFetchError";
-import {ContentLengthError} from "../utils/errors/ContentLengthError";
-import {GithubApiError} from "../utils/errors/GithubApiError";
-import {NoContentError} from "../utils/errors/NoContentError";
+import githubService from "../services/github-service"
+import fetchProjectsFromMp from "../services/mp-projects-fetcher"
+import { ContentLengthError } from "../utils/errors/ContentLengthError"
+import { GithubApiError } from "../utils/errors/GithubApiError"
+import { MPFetchError } from "../utils/errors/MPFetchError"
+import { NoContentError } from "../utils/errors/NoContentError"
+import { Definition, Project } from "../utils/structures"
 
 interface StudyPageProps {
-  definitions: string;
-  projectExists: boolean;
-  exceptionMessage: string;
-  exceptionStatusCode: number;
+  definitions: string
+  projectExists: boolean
+  exceptionMessage: string
+  exceptionStatusCode: number
 }
 
 // Renders the eligibility page
-const Study: NextPage<StudyPageProps> = ({definitions, projectExists, exceptionMessage, exceptionStatusCode}) => {
+const Study: NextPage<StudyPageProps> = ({
+  definitions,
+  projectExists,
+  exceptionMessage,
+  exceptionStatusCode,
+}) => {
   const router = useRouter()
   const [projectId, setProjectId] = useState<string | null>(null)
   const studyInfo: MutableRefObject<Definition[]> = useRef([])
@@ -30,7 +35,7 @@ const Study: NextPage<StudyPageProps> = ({definitions, projectExists, exceptionM
   useEffect(() => {
     // If the router is not ready yet, or we already have a flow, do nothing.
     if (router.isReady) {
-      const {projectId} = router.query
+      const { projectId } = router.query
       if (definitions != null) {
         studyInfo.current = JSON.parse(definitions) as Definition[]
       }
@@ -41,23 +46,28 @@ const Study: NextPage<StudyPageProps> = ({definitions, projectExists, exceptionM
     }
   }, [router.query, router.isReady, definitions])
 
-  if(exceptionMessage) {
+  if (exceptionMessage) {
     return (
-        <MarginCard>
-          <CardTitle>An exception occurred while fetching the project or definitions</CardTitle>
-          <p>{exceptionMessage}</p>
-          {exceptionStatusCode && <p>Status Code: {exceptionStatusCode}</p>}
-        </MarginCard>
+      <MarginCard>
+        <CardTitle>
+          An exception occurred while fetching the project or definitions
+        </CardTitle>
+        <p>{exceptionMessage}</p>
+        {exceptionStatusCode && <p>Status Code: {exceptionStatusCode}</p>}
+      </MarginCard>
     )
   }
 
   if (projectExists === false) {
     return (
-        <MarginCard>
-          <CardTitle>Project Not Found</CardTitle>
-          <p>The project with the name {projectId} does not exist in the Management Portal.</p>
-          <p>Please enter a valid project name.</p>
-        </MarginCard>
+      <MarginCard>
+        <CardTitle>Project Not Found</CardTitle>
+        <p>
+          The project with the name {projectId} does not exist in the Management
+          Portal.
+        </p>
+        <p>Please enter a valid project name.</p>
+      </MarginCard>
     )
   }
 
@@ -103,50 +113,59 @@ const StudyInfo: React.FC<any> = ({ questions }) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const {projectId} = context.query
+  const { projectId } = context.query
   if (typeof projectId === "string") {
     try {
       const fetchedProjects: Project[] = await fetchProjectsFromMp()
-      console.log(fetchedProjects)
-      const mpProjectNames: string[] = fetchedProjects.map((project: Project) => project.projectName)
+      const mpProjectNames: string[] = fetchedProjects.map(
+        (project: Project) => project.projectName,
+      )
       if (!mpProjectNames.includes(projectId)) {
         console.log("No project found")
         return {
           props: {
-            projectExists: false
-          }
-        };
+            projectExists: false,
+          },
+        }
       }
 
-      const studyDefinitions: string | undefined = await githubService.initiateFetch(projectId,
-          REMOTE_DEFINITIONS_CONFIG.STUDY_INFO_DEFINITION_FILE_NAME_CONTENT, REMOTE_DEFINITIONS_CONFIG.STUDY_INFO_VERSION)
+      const studyDefinitions: string | undefined =
+        await githubService.initiateFetch(
+          projectId,
+          REMOTE_DEFINITIONS_CONFIG.STUDY_INFO_DEFINITION_FILE_NAME_CONTENT,
+          REMOTE_DEFINITIONS_CONFIG.STUDY_INFO_VERSION,
+        )
 
-return studyDefinitions === undefined 
-  ? { props: { projectExists: true } } 
-  : { props: { definitions: studyDefinitions, projectExists: true } }
+      return studyDefinitions === undefined
+        ? { props: { projectExists: true } }
+        : { props: { definitions: studyDefinitions, projectExists: true } }
     } catch (error: any) {
-      if (error instanceof MPFetchError || error instanceof ContentLengthError || error instanceof NoContentError) {
-        return {
-          props: {
-            exceptionMessage: error.message
-          }
-        }
-      } else if (error instanceof  GithubApiError) {
+      if (
+        error instanceof MPFetchError ||
+        error instanceof ContentLengthError ||
+        error instanceof NoContentError
+      ) {
         return {
           props: {
             exceptionMessage: error.message,
-            exceptionStatusCode: error.statusCode
-          }
+          },
+        }
+      } else if (error instanceof GithubApiError) {
+        return {
+          props: {
+            exceptionMessage: error.message,
+            exceptionStatusCode: error.statusCode,
+          },
         }
       } else {
         return {
           props: {
-            exceptionMessage: error.message
-          }
+            exceptionMessage: error.message,
+          },
         }
       }
     }
-  } else return {props: {projectExists: false}}
+  } else return { props: { projectExists: false } }
 }
 
 export default Study
--- utils/errors/MPFetchError.ts
+++ utils/errors/MPFetchError.ts
@@ -1,11 +1,10 @@
 export class MPFetchError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = "MPFetchError"
 
-    constructor(message: string) {
-        super(message);
-        this.name = "MPFetchError";
-
-        if (Error.captureStackTrace) {
-            Error.captureStackTrace(this, MPFetchError);
-        }
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, MPFetchError)
     }
+  }
 }
