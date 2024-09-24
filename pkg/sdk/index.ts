eac3306baf031f333ee7cf1bdef13249029982a4
Pauline <mpgxvii@gmail.com>
2024-09-24 10:57:54 +0100

    Rename variables

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -2,7 +2,7 @@ import { Configuration, FrontendApi } from "@ory/client"
 import { edgeConfig } from "@ory/integrations/next"
 
 const localConfig = {
-  basePath: process.env.NEXT_PUBLIC_KRATOS_PUBLIC_URL,
+  basePath: process.env.KRATOS_PUBLIC_URL,
   baseOptions: {
     withCredentials: true,
   },
@@ -10,6 +10,6 @@ const localConfig = {
 
 export default new FrontendApi(
   new Configuration(
-    process.env.NEXT_PUBLIC_KRATOS_PUBLIC_URL ? localConfig : edgeConfig,
+    process.env.KRATOS_PUBLIC_URL ? localConfig : edgeConfig,
   ),
 )
