f0a1423bec1d4200fa4d49c0fffb3aa9ab3041eb
Pauline <mpgxvii@gmail.com>
2024-09-28 21:42:47 +0100

    Fix formatting

--- pages/health/alive.ts
+++ pages/health/alive.ts
@@ -1,5 +1,5 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).send("ok");
+  return res.status(200).send("ok")
 }
