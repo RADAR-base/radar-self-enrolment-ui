fceb65776defbbc27395f64a922d98f9943aff00
Callum Stewart <callum.stewart@kcl.ac.uk>
2025-01-14 14:01:39 +0000

    rest source connect service

--- app/_lib/radar/rest-source/service.ts
+++ app/_lib/radar/rest-source/service.ts
@@ -1,4 +1,10 @@
 const USER_ENDPOINT = ''
+const AUTH_BASE_URL = process.env.NEXT_PUBLIC_HYDRA_PUBLIC_URL + "/oauth2"
+const GRANT_TYPE = "authorization_code"
+const CLIENT_ID = process.env.ARMT_CLIENT_ID ?? "aRMT"
+const CLIENT_SECRET = process.env.ARMT_CLIENT_SECRET  ?? ""
+const REDIRECT_URI = process.env.NEXT_PUBLIC_REST_REDIRECT_URI
+
 
 async function getRestSourceUser(
   accessToken: string,
@@ -39,4 +45,18 @@ async function getRestSourceUser(
     console.error(error)
     return null
   }
+}
+
+
+export function authRequestLink(state: string): string {
+  const scopes = [
+    "SOURCETYPE.READ",
+    "PROJECT.READ",
+    "SUBJECT.READ",
+    "SUBJECT.UPDATE",
+    "SUBJECT.CREATE",
+  ].join("%20")
+  const audience = "res_restAuthorizer"
+  const authUrl = `${AUTH_BASE_URL}/auth?client_id=${CLIENT_ID}&response_type=code&state=${state}&audience=${audience}&scope=${scopes}&redirect_uri=${REDIRECT_URI}`
+  return authUrl
 }
