97233ff0c38c53be8f577f785b98d6cd197b2cb5
Callum Stewart <callum.stewart@kcl.ac.uk>
2025-02-04 13:23:02 +0000

    remove protocol param for devicespanel (uses context instead)

--- app/[studyId]/portal/connect/page.tsx
+++ app/[studyId]/portal/connect/page.tsx
@@ -1,14 +1,8 @@
 "use server"
 import { Box, Container } from '@mui/material';
-import StudyProtocolRepository from '@/app/_lib/study/protocol/repository';
-import { ArmtDefinitionRepository, getDefinition } from '@/app/_lib/armt/repository/repository';
 import { DevicesPanel } from '@/app/_ui/portal/devicesPanel';
 
-
 export default async function Page({ params }: { params: { studyId: string} }) {
-  const registery: StudyProtocolRepository = new StudyProtocolRepository()
-  const protocol = await registery.getStudyProtocol(params.studyId)
-  const armtRepo = new ArmtDefinitionRepository(protocol)
   return (
     <main>
       <Box sx={{ flexGrow: 1, margin: {xs: 0, sm: 2}}} 
@@ -17,7 +11,7 @@ export default async function Page({ params }: { params: { studyId: string} }) {
             justifyContent="center"
             alignItems="center">
         <Container maxWidth="lg" disableGutters>
-          <DevicesPanel protocol={protocol}></DevicesPanel>
+          <DevicesPanel />
         </Container>
       </Box>
     </main>
