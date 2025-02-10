6ba06503e362a2476f57a444f391ff3d390da294
Callum Stewart <callum.stewart@kcl.ac.uk>
2025-02-10 12:56:53 +0000

    remove roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { CssBaseline, ThemeProvider } from "@mui/material";
