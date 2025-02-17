d9b6840e2ed5cd5812c50a30517db135a6175ca7
Callum Stewart <callum.stewart@kcl.ac.uk>
2025-02-17 19:23:28 +0000

    redcap definition evaluated branching logic

--- app/_ui/components/fields/date.tsx
+++ app/_ui/components/fields/date.tsx
@@ -3,26 +3,38 @@ import { ForwardedRef } from "react";
 import { IDateItem } from "@/app/_lib/armt/definition/field.interfaces";
 import { DatePicker, DateView } from "@mui/x-date-pickers";
 import dayjs from 'dayjs';
-
+import 'dayjs/locale/en-gb';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 
-function formatFromView(view: DateView[]) {
-
+function valueFormatFromView(view: DateView[]) {
+  let formatArray: string[] = []
+  if (view.includes("year")) {
+    formatArray.push("YYYY")
+  }
+  if (view.includes("month")) {
+    formatArray.push("MM")
+  }
+  if (view.includes("day")) {
+    formatArray.push("DD")
+  }
+  return formatArray.join("-")
 }
 
 interface ArmtDateFieldProps extends IDateItem {
-  setFieldValue: (id: string, value: Date) => void
-  value?: Date,
+  setFieldValue: (id: string, value: string | Date) => void
+  value?: string | Date,
   disabled?: boolean,
   key?: string,
   color? : 'primary' | 'secondary' | 'standard' | 'error' | 'info' | 'success' | 'warning'
 }
 
 export function ArmtDateField({label, description, errorText, ...props}: ArmtDateFieldProps, ref: ForwardedRef<HTMLDivElement>) {
+  const view = (props.views ?? ["year", "month", "day"]) as DateView[]
+  const valueFormat = valueFormatFromView(view)
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'en-gb'}>
       <Box display={"flex"} flexDirection={"column"} textAlign={"left"}>
         <Typography variant="h4" component={'span'}>{props.title}</Typography>
         <Typography variant="body1" component={'span'}>{description}</Typography>
@@ -31,21 +43,20 @@ export function ArmtDateField({label, description, errorText, ...props}: ArmtDat
             maxWidth: '16em'
           }}
           key={props.key}
-          value={props.value ? dayjs(props.value) : null}
+          value={props.value ? dayjs(props.value, valueFormat) : null}
           disabled={props.disabled}
           openTo="year"
-          views={(props.views ?? ['year','month', 'day']) as DateView[]}
+          views={view}
           formatDensity='spacious'
           label={label}
           onChange={(value, context) => {
             if (value) {
               try {
-              props.setFieldValue(props.id, value.toDate())
+              props.setFieldValue(props.id, value.format(valueFormat))
               } catch {
               }
             }
           }}
-          
         />
         <Typography variant="overline" component={'span'} color="error">{errorText}</Typography>
       </Box>
