ed4f7422b4f60c415e49dc57b6e6cc5a0f6890ef
Pauline <mpgxvii@gmail.com>
2025-03-17 15:19:44 +0000

    Remove comments and update hydra url env var name

--- app/[studyId]/page.tsx
+++ app/[studyId]/page.tsx
@@ -1,13 +1,6 @@
 import PageRepository from "@/app/_lib/study/siteContent/repository";
 import { BlockPage } from "@/app/_ui/components/blocks/blockPage";
 
-// export const dynamicParams = true
-
-// export async function generateStaticParams() {
-//   const registery = new ProtocolRepository()
-//   return await (registery.getStudies().then(studies => studies.map((id) => {studyId: id})))
-// }
-
 export default async function Page({ params }: { params: { studyId: string } }) {
   
   var pageRegistry: PageRepository = new PageRepository()
