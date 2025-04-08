5d3b8e35a041c90f1a0a2f91a3d05a23b24fef61
Pauline <mpgxvii@gmail.com>
2025-04-08 09:21:38 +0100

    Allow no auth for public client like aRMT

--- app/_lib/connect/token.ts
+++ app/_lib/connect/token.ts
@@ -1,6 +1,8 @@
 "use server"
 
 const AUTH_BASE_URL = process.env.HYDRA_PUBLIC_URL + "/oauth2"
+const ARMT_CLIENT_ID = process.env.ARMT_CLIENT_ID ?? 'aRMT'
+const SEP_CLIENT_ID = process.env.SEP_CLIENT_ID ?? 'SEP'
 const GRANT_TYPE = "authorization_code"
 
 interface GetAccessTokenParams {
@@ -11,22 +13,32 @@ interface GetAccessTokenParams {
 }
 
 export default async function getAccessToken(
-  { code, redirectUri, clientId, clientSecret }: GetAccessTokenParams):
-  Promise<any> {
+  { code, redirectUri, clientId, clientSecret }: GetAccessTokenParams
+): Promise<any> {
   const bodyParams = new URLSearchParams({
     grant_type: GRANT_TYPE,
     code,
-    redirect_uri: redirectUri
+    redirect_uri: redirectUri,
+    client_id: clientId
   })
 
-  try {
+  if (clientId !== ARMT_CLIENT_ID) {
+    bodyParams.append("client_secret", clientSecret)
+  }
+
+  const headers: Record<string, string> = {
+    "Content-Type": "application/x-www-form-urlencoded",
+  }
+
+  if (clientId !== "aRMT") {
     const auth = Buffer.from(`${clientId}:${clientSecret}`).toString("base64")
+    headers["Authorization"] = `Basic ${auth}`
+  }
+
+  try {
     const response = await fetch(`${AUTH_BASE_URL}/token`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": `Basic ${auth}`
-      },
+      headers,
       body: bodyParams,
     })
 
