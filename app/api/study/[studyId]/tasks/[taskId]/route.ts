a9d96484d17ad353f0f0b48a94ce50541e3126d9
Callum Stewart <callum.stewart@kcl.ac.uk>
2025-05-27 21:37:29 +0100

    update task api route

--- app/api/study/[studyId]/tasks/[taskId]/route.ts
+++ app/api/study/[studyId]/tasks/[taskId]/route.ts
@@ -21,6 +21,92 @@ async function sendTaskResponse(studyId: string, userId: string, taskData: Activ
   })
 }
 
+async function getCurrentValue(studyId: string, userId: string, taskId: string) {
+  const resp = await fetch(process.env.KRATOS_ADMIN_URL + '/identities/' + userId)
+  if (resp.ok) {
+    try {
+      const userData = await resp.json()
+      const currentValue = userData['metadata_admin']['study'][studyId][taskId]
+      console.log('curr val: ', currentValue)
+      return currentValue
+    } catch {
+      return null
+    }
+  }
+
+  return null
+
+}
+
+export async function PUT(request: NextRequest,
+  { params }: { params: Promise<{ studyId: string, taskId: string }> }
+) {
+  const {studyId, taskId}  = (await params)
+  let oryUser: any
+  let userId: string
+  let taskData: ActiveTaskResponse
+
+  const registery: StudyProtocolRepository = new StudyProtocolRepository()  
+  const protocol = await registery.getStudyProtocol(studyId)
+  if (protocol == undefined) {
+    return NextResponse.json({error: {type: 'request', content: 'No such study exists'}}, {status: 400})
+  }
+
+  try {
+    const resp = await whoAmI()
+    if (resp.status != 200) {
+      return NextResponse.json({error: {type: 'authentication', content: {message: "No user session"}}}, {status: 403})
+    }
+    oryUser = await resp.json()
+    userId = oryUser['identity']['id']
+  } catch {
+    return NextResponse.json({error: {type: 'authentication', content: {message: "Error decoding user session"}}}, {status: 403})
+  }
+
+  const armtRepo = new ArmtDefinitionRepository(protocol)
+  const taskProtocol = (protocol.protocols.find((e) => e.id == taskId))
+  
+  if (taskProtocol == undefined) {
+    return NextResponse.json({error: {type: 'request', content: 'No such task'}} , {status: 400})
+  }
+
+  var previousValues = (await getCurrentValue(studyId, userId, taskId)) ?? {}
+  var newValues = (await request.json())
+  const values = Object.assign({}, newValues, previousValues)
+
+
+  if (taskProtocol.metadata.type == "redcap_local") {
+    const redcapDef = await armtRepo.getDefinition(taskId)
+    if (redcapDef == undefined) {
+      return NextResponse.json({error: {type: 'request', content: 'No such task'}} , {status: 400})
+    }
+    const armtDef = fromRedcapDefinition(redcapDef)
+    const schema = schemaFromDefinition(armtDef)
+    try {
+      await schema.validate(values)
+    } catch (error) {
+      return NextResponse.json({error: {type: 'validation', content: error}} , {status: 400})
+    }
+    taskData = {
+      'id': armtDef.id,
+      'name': armtDef.name,
+      'time': Date.now(),
+      'version': '',
+      'answers': values
+    }
+  } else {
+    taskData = {
+      'id': taskId,
+      'name': taskId,
+      'time': Date.now(),
+      'version': '',
+      'answers': values
+    }
+  }
+  await sendTaskResponse(studyId, userId, taskData)
+  return new NextResponse(null, {status: 200})
+}
+
 export async function POST(
   request: NextRequest,
   { params }: { params: Promise<{ studyId: string, taskId: string }> }
@@ -53,6 +139,7 @@ export async function POST(
   if (taskProtocol == undefined) {
     return NextResponse.json({error: {type: 'request', content: 'No such task'}} , {status: 400})
   }
+
   if (taskProtocol.metadata.type == "redcap_local") {
     const redcapDef = await armtRepo.getDefinition(taskId)
     if (redcapDef == undefined) {
