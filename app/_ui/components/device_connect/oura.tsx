9c2266be46a64199de0833fa87dd5a3d5af5b95f
Callum Stewart <callum.stewart@kcl.ac.uk>
2025-09-24 10:56:08 +0100

    Guide updates 24-Sep-25

--- app/_ui/components/device_connect/oura.tsx
+++ app/_ui/components/device_connect/oura.tsx
@@ -23,7 +23,7 @@ export function OuraPage() {
                 The first step will take you to Oura's website. For the second step <strong>you need to login to your Oura account.</strong> In the third step you will be asked to tick all the 3 boxes in the “Connect with Oura” screen.  
               </Typography>
               <Typography>
-                Read our <Link component={NextLink} href={'/study/paprka/resources/guides/PAPrKA_Study_Guide_Oura.docx'}>Guide</Link> or view our <Link>Video</Link> for more detailed instructions on how to share your Oura data.  
+                Read our <Link component={NextLink} href={'/study/paprka/resources/guides/PAPrKA_Study_Guide_Oura.pdf'}>Guide</Link> or view our <Link>Video</Link> for more detailed instructions on how to share your Oura data.  
               </Typography>
             </div>
           </Grid>
